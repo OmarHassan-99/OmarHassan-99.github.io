2352d0803bce836df810bdf01dc70927ff4e5f65
Omar <oh777889@gmail.com>
2026-06-17 21:40:01 +0200

    fixed dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/OmarHassan-99.github.io/', // Replace with your actual GitHub repo name
 })
